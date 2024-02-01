8d72ae8992534cd05c136847ae0aa2b10fe30f52
WinnardArthur <winnardarthur7@gmail.com>
2024-02-01 21:48:52 +0000

    implement delete server feature

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -13,7 +13,7 @@ export async function PATCH(
     const { name, imageUrl } = await req.json();
 
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
- 
+
     if (!params.serverId) {
       return new NextResponse("Server Id is required", { status: 401 });
     }
@@ -32,6 +32,33 @@ export async function PATCH(
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
+    throw new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!params.serverId) {
+      return new NextResponse("Server Id is required", { status: 401 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
     throw new NextResponse("Internal Error", { status: 500 });
   }
 }
