c4f199989874db746e009e0040c1717c3d5ab876
WinnardArthur <winnardarthur7@gmail.com>
2024-02-03 06:38:04 +0000

    popup default channel

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -33,6 +33,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z
@@ -45,20 +46,29 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
 
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
+  const { channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
@@ -70,7 +80,7 @@ export const CreateChannelModal = () => {
           serverId: params?.serverId,
         },
       });
-      await axios.post(url, values);  
+      await axios.post(url, values);
 
       form.reset();
       onClose();
