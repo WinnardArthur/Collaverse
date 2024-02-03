03c7c53eb47744faefee85eec4d8ffe310f01188
WinnardArthur <winnardarthur7@gmail.com>
2024-02-03 09:47:01 +0000

    create initial pages and redirect to general when a server is initially clicked

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const ServerIdPage = () => {
-  return (
-    <div>ServerIdPage</div>
-  )
+interface ServerIdPage {
+  params: {
+    serverId: string;
+  };
 }
 
-export default ServerIdPage
+const ServerIdPage = async ({ params }: ServerIdPage) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+};
+
+export default ServerIdPage;
